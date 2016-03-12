573be2264e2d6956344ed2dff5abe465762f00ea
AurelGit <aurelien.mc@gmail.com>
2016-03-12 09:32:39 +0100

    suppression speed icons ajout fa

--- fuel/app/views/template.php
+++ fuel/app/views/template.php
@@ -5,10 +5,10 @@
 	<meta charset="utf-8">
 
 	<title><?php echo isset($title) ? $title : null; ?></title>
-	<meta name="author" content="SpeedMedia Services">
+	<meta name="author" content="Aurélien">
 	
 	<?php echo Asset::css('foundation.min.css'); ?>
-	<?php echo Asset::css('icons.css'); ?>
+	<?php echo Asset::css('vendor/fa/font-awesome.min.css'); ?>
 	
 	<?php echo Asset::js('jquery-2.2.0.min.js'); ?>
 </head>
